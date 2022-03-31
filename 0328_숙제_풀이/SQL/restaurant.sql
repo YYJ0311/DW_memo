a7754a52fe7bb2a0eb6675dad4a9d7de7ce27a89
YYJ0311 <dudwns0311@naver.com>
2022-04-01 06:03:12 +0900

    fix : 문제풀이

--- 0328_숙제_풀이/SQL/restaurant.sql
+++ 0328_숙제_풀이/SQL/restaurant.sql
@@ -11,6 +11,7 @@ CREATE TABLE restaurant(
     foodNo INTEGER(4) NOT NULL COMMENT '음식 번호',
     FOREIGN KEY(foodNo) REFERENCES food(foodNo)
 );
+-- 레스토랑 테이블에서 음식 테이블의 음식번호를 참조중임
 
 SELECT * FROM restaurant WHERE foodNo = 30 AND restaurantName= '빠스타스';
 /* 문제 1.
@@ -18,6 +19,8 @@ SELECT * FROM restaurant WHERE foodNo = 30 AND restaurantName= '빠스타스';
 레스토랑 테이블에서 위에 작성한 select쿼리 호출 시간이 오래 걸리기 시작했다.
 가장 먼저해야하는 바람직한 행동은?
 답안 작성 =>
+    1. * 으로 전체 호출하지 말고 필요한 컬럼만 골라서 호출한다
+    2. 자주 사용하는 컬럼에 INDEX를 생성한다
 */
 
 DELETE FROM food
@@ -27,4 +30,7 @@ WHERE foodNo = 30;
 하지만 레스토랑 테이블에 30번 데이터가 있어서 지울 수가 없다. 
 음식 테이블에 30번 데이터를 지울 방법은 무엇일까?
 답안 작성 =>
+    ON DELETE CASCADE 를 사용해서 부모테이블과 자식테이블 데이터를 같이 지운다
+    ON DELETE CASCADE는 테이블을 생성할 때 적용하므로 이미 존재하는 자식테이블(레스토랑 테이블)에 적용하기 위해서는,
+    ALTER 명령어를 이용해서 기존의 FK를 지웠다가 ON DELETE CASCADE를 추가한 FK를 다시 생성하면 된다.
 */
--- 0328_숙제_풀이/algorithm/index.js
+++ 0328_숙제_풀이/algorithm/index.js
@@ -2,6 +2,16 @@
 // 버블정렬을 이용하여 오름차순으로 정렬하고 총 몇회전 했는지 알아내시오.
 let array = [1, 10, 4, 3, 5];
 let count = 0; //총 몇회전 했는지 알아내는 변수
-
+let temp = [];
+for(var i=0; i<array.length; i++){
+    for(var j=0; j<array.length-1; j++){
+        if(array[i]<array[j]){
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        count++
+    }
+}
 console.log(array);
 console.log(count);
